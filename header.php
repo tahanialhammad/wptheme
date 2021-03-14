81228738e1bb36618bfbdd7ce3242d1f0e14c98c
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-14 23:32:46 +0100

    add menu function

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <header>
         Header
         <div class="container">
+        <h1>Logo</h1>
             <nav>
                 <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
             </nav>
