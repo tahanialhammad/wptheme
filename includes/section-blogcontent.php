5bcafa9072857f73cf93231d8f6b5dc8f21e29af
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-15 21:21:13 +0100

    add theme option image thumbnail with customsize

--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -1,34 +1,35 @@
 <!--single post-->
-<?php if(have_posts()):while(have_posts()):the_post(); ?>
-<!-- this is a single page template  -->
-<?= get_the_date(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <!-- this is a single page template  -->
+        <?= get_the_date(); ?>
 
-<?php the_content(); ?>
+        <?php the_content(); ?>
 
-<!-- <?php the_author(); ?> -->
-<?php 
-$fname = get_the_author_meta( 'first_name');
-$lname = get_the_author_meta( 'last_name');
-// echo $fname . '-' .$lname;
-?>
-<p>Posted by: <?= $fname; ?> - <?= $lname; ?></p>
+        <!-- <?php the_author(); ?> -->
+        <?php
+        $fname = get_the_author_meta('first_name');
+        $lname = get_the_author_meta('last_name');
+        // echo $fname . '-' .$lname;
+        ?>
+        <p>Posted by: <?= $fname; ?> - <?= $lname; ?></p>
 
-<?php
-$tags = get_the_tags();
-foreach($tags as $tag): ?>
-<a href=" <?= get_tag_link( $tag->term_id ); ?>" class="badge bg-success">
-    <?= $tag->name; ?>
-</a>  
-<?php endforeach; ?>
+        <?php
+        $tags = get_the_tags();
+        foreach ($tags as $tag) : ?>
+            <a href=" <?= get_tag_link($tag->term_id); ?>" class="badge bg-success">
+                <?= $tag->name; ?>
+            </a>
+        <?php endforeach; ?>
 
-<?php
-$categories= get_the_category();
-foreach($categories as $cat): ?>
-    <a href=" <?= get_category_link( $cat->term_id ); ?>" class="badge bg-primary">
-        <?= $cat->name; ?>
-    </a>
-<?php endforeach; ?>
+        <?php
+        $categories = get_the_category();
+        foreach ($categories as $cat) : ?>
+            <a href=" <?= get_category_link($cat->term_id); ?>" class="badge bg-primary">
+                <?= $cat->name; ?>
+            </a>
+        <?php endforeach; ?>
 
-<?php comments_template(); ?>
+        <?php comments_template(); ?>
 
-<?php endwhile; else: endif; ?>
+<?php endwhile;
+else : endif; ?>
