a2fc04b1486fc7ad81118cdf0bf5e231ec85bbc2
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-16 23:23:56 +0100

    add sidebar option

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ add_action('wp_enqueue_scripts','load_js');
 //Theme options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+add_theme_support('widgets');
 
 //Menus
 register_nav_menus(array(
@@ -27,3 +28,26 @@ register_nav_menus(array(
 //Custom  images size
 add_image_size( 'blog-large', 800, 400, false );
 add_image_size( 'blog-small', 300, 200, true );
+
+
+//Register Sidebars
+function my_sidebars()
+{
+    register_sidebar(
+        array(
+            'name'=>'Page Sidebar',
+            'id'=>'page-sidebar',
+            'before_title'=>'<h4 class="weidgets-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'=>'Blog Sidebar',
+            'id'=>'blog-sidebar',
+            'before_title'=>'<h4 class="weidgets-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+}
+add_action( 'widgets_init', 'my_sidebars');
