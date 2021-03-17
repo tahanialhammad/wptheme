6ca0cf6701001e41f5235d14bf0f04474af16571
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-17 20:48:16 +0100

    add search form

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@ The Front page template
 The front page template
     <h1><?php the_title(); ?></h1>
     <?php get_template_part('includes/section', 'content') ?>
+    <?php get_search_form(); ?>
 </div>
 
 <?php get_footer(); ?>
