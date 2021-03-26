2ce8157b64d56187790218e9dcf81ea160d544f7
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-26 14:49:02 +0100

    add Bootstrap Navbar and custom logo

--- header.php
+++ header.php
@@ -8,14 +8,37 @@
 </head>
 
 <body>
+<?php 
+if (has_custom_logo()){
+    $custom_logo_id = get_theme_mod('custom_logo');
+    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+    
+    $logo = '<a href="'.get_bloginfo('url').'" target="_self" >
+    <img src="'.$logo[0].'" alt="'.get_bloginfo('name').'" title="'.get_bloginfo('name').'"></a>';
+}else{
+    $logo= '<h3>'.get_bloginfo('name').'</h3>';
+};
+?>
     <header>
-        Header
-        <div class="container">
-        <nav>  
-            <?php wp_nav_menu(array(
-            'theme_location'=>'main-menu',
-            'menu_class'=>'main-menu'
-            )); ?>
+        <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?= $logo?></a>
+                <?php
+                wp_nav_menu(array(
+                    'theme_location'    => 'main-menu',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'bs-example-navbar-collapse-1',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ));
+                ?>
+            </div>
         </nav>
-        </div>
     </header>
