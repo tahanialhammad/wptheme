02d081b966b430ac821457be8b630565d118243c
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-28 18:28:29 +0200

    post style

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -5,10 +5,13 @@
                 <img src="<?php the_post_thumbnail_url('blog-small'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-3 img-thumbnail mr-4">
             <?php endif; ?>
             <div>
-                <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <h4 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                <span class=" text-muted"><i class="fas fa-user"></i> <?php the_author(); ?>, </span>
+                <span class=" text-muted"><i class="far fa-calendar-alt"></i><?= get_the_date(); ?>, </span>
+                <span class=" text-muted"><i class="far fa-comment"></i><?php comments_number(); ?>, </span>
+
                 <p class="card-text"><?php the_excerpt(); ?></p>
                 <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more <i class="fas fa-arrow-right"></i> </a>
-                <p><?php comments_number(); ?></p>
             </div>
         </div>
     </div>
